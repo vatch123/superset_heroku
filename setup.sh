f3f4364b4a3d5fc0406848eca5adeda96fe86fd4
vatch123 <vatsalyachaubey19980@gmail.com>
2021-07-17 13:53:28 +0530

    fix

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-pip libsasl2-dev libldap2-dev
 
-pip install -r requirements.txt
-
 python3 -m venv superset
 
 . superset/bin/activate
 
-pip install apache-superset
+pip install -r requirements.txt
 
 superset db upgrade
 
