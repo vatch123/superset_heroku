ad905caac72f2534af04b97e34be698419b46ab0
vatch123 <vatsalyachaubey19980@gmail.com>
2021-07-17 15:06:18 +0530

    fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-pip libsasl2-dev libldap2-dev
+apt-get install build-essential libssl-dev libffi-dev python-dev python-pip libsasl2-dev libldap2-dev
 
 python3 -m venv superset
 
 . superset/bin/activate
 
 pip install -r requirements.txt
+
+export FLASK_APP=superset
